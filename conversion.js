d183d4eae5181bab79c82febc45b4786deb3b6a0
Eric Peers <peers@mtnboy.net>
2017-03-10 22:21:04 -0700

    showing a failing test.

--- conversion.js
+++ conversion.js
@@ -1,9 +1,11 @@
 /*
-// Conversion Class - for Avoka technical Assessment
+// Conversion Class - for Technical Assessment
 //
 // This class provides a Fluent Interface (aka chaining) to convert between like units
-// of measurement. It will generate a TypeException if the wrong arguments are presented, and a
-// XXXException if the conflicting arguments (e.g. kg to feet) are presented.
+// of measurement. It will generate a TypeError if the wrong arguments are presented, and a
+// TypeError if conflicting arguments (e.g. kg to feet) are presented.
+//
+// It also throws a Syntax error if you give it something like "Frozbozz" as a type.
 //
 // Valid conversions are:
 //
@@ -17,8 +19,12 @@
 //       .to("kg")
 //       .execute()
 //
+// Assumptions:
+//   * Your conversions will fit within a numeric representationi n Node/JS - 51 bits of precision in floats/mantissa!
+//   * You are sending down a string as per the original
 // Note that the Fluent/chaining functions (convert, to) will return an instance of Conversion, and execute will
 //  return a n.2 float, rounded to the neared 100th place.
+// N
 
 */
 
@@ -44,7 +50,7 @@ Conversion.TYPE = {
   VOLUME: 3
 }
 
-//this allows a lookup based on source.destination in a hash to get the conversion constant.
+// this allows a lookup based on source.destination in a hash to get the conversion constant.
 // normally I could just convert from A to B. But a smaller data structure (with less room for
 // error) allows me to convert from a A to Z to B. The advantage here is going to a normalized
 // unit, and then all I have to do is convert from normalized to my final unit.
@@ -53,6 +59,10 @@ Conversion.TYPE = {
 // scale with a huge number of conversions. Basically if I had 30 different types of weight, then
 // I would need a 30x30 matrix to handle the conversions, and that's starting to be inefficient. I can
 // store it in a hash which gives me o(1) lookup, but still...
+//
+// another interesting issue, which I didn't encounter, but could be an issue with floats, is that if
+// I convert using imprecisely represented conversions, then I might somehow say that 24 inches = 1.999999999999 feet.
+// there's a trick I employ below to try to avoid this.
 Conversion.normalizers = {};
 //convert all weight to kilos
 Conversion.normalizers[Conversion.UNIT.POUND] = 1.0/2.2;
@@ -78,6 +88,9 @@ function Conversion () {
   return this;
 }
 
+//to make this extensible, we could create either a regexp engine or create a set of hashes that convert, and then
+//just lookup the result directly. Might be a simpler routine. But frankly, KISS for right now and don't use indirection when
+//the problem isn't large. Deal with that sort of refactor when you need it.
 Conversion.prototype.validateUnit = function(unitStr) {
   var self = this;
 
@@ -190,10 +203,28 @@ Conversion.prototype.execute = function() {
   } else if (self.targetUnit.type != self.sourceUnit.type) {
     throw new TypeError("Target type != Source type");
     return null;
+
+
   } else {
-    //convert the item into the our "metric" equivalent, and then invert that to the target of interest.
-    var intermediate = Conversion.normalizers[self.sourceUnit.unit] * self.sourceValue;
-    return intermediate / Conversion.normalizers[self.targetUnit.unit];
+    //do the actual conversion.
+
+
+    /* note - original implementation did this - but it could cause imprecision in floats. Instead, I'll create a conversion ratio.
+
+      //convert the item into the our "metric" equivalent, and then invert that to the target of interest.
+      var intermediate = Conversion.normalizers[self.sourceUnit.unit] * self.sourceValue;
+      return intermediate / Conversion.normalizers[self.targetUnit.unit];
+    */
+
+    var conversion_ratio = Conversion.normalizers[self.sourceUnit.unit] / Conversion.normalizers[self.targetUnit.unit];
+    var result = self.sourceValue * conversion_ratio;
+
+    // I'd prefer to send the full float down, so somebody else consuming it can retain whatever precision I still have
+    // and make a final decision. But it's in the
+    // requirements, so we're doing it. I'd actually go back to product/redefine the requirements in real life, but F'ake la vie.
+    // http://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places
+    var rounded = +result.toFixed(2);
+    return rounded;
   }
 }
 
--- spec/convertSpec.js
+++ spec/convertSpec.js
@@ -10,6 +10,7 @@ describe("converty", function () {
     expect(result).toBe(1, "Want the same result out as I put in");
 
   });
+
   it("should let me convert inches to feet", function () {
     var result = new Conversion().convert(12, "inches").to("feet").execute();
     expect(result).toBe(1, "Want 1 foot coming out");
@@ -21,14 +22,37 @@ describe("converty", function () {
   });
 
   it ("should throw an exception when I don't supply a .to", function () {
+    //playing with syntax in interrupting the call chain here too.
     var halfsy = new Conversion().convert(1, "bomber");
+
+    //http://stackoverflow.com/questions/4144686/jasmine-how-to-write-a-test-which-expects-an-error-to-be-thrown
+    //have to put the item that will generate the exception inside of a function so that it defers the calling.
     expect( function () { halfsy.execute(); } ).toThrow(new Error("Failed to supply a target unit"));
   });
 
-  xit ("should throw an exception when I don't supply a .convert", function () {
-  });
-  xit ("should throw an exception when I don't supply a .convert or a .to", function () {
+  it ("should throw an exception when I don't supply a .convert", function () {
+    expect( function () {
+      var cv = new Conversion().to("pints").execute();
+    }).toThrow(new Error("Failed to supply a source unit"))
   });
 
+  it ("should throw an exception with a null value", function () {
+    expect( function () {
+      var cv = new Conversion().convert(null, "feet").to("inch");
+      console.log(cv);
+    }).toThrow(new Error("No source value set"))
+  });
+
+  it ("should throw an exception when I don't supply a .convert or a .to", function () {
+    expect( function () {
+      new Conversion().execute();
+    }).toThrow(new TypeError("Failed to supply a source unit"))
+
+  });
+  xit ("should complain if I convert Frozbozz to Fizbangs... Or variable types");
+
+  //do a conversion with long result.
+  //also do an identity conversion which will truncate.
+  xit ("should truncate a super long float to 2 digits of precision");
 
 });
