d868e729b49fa4bdc1b599fb6a218271f4e58513
Eric Peers <peers@mtnboy.net>
2017-03-10 21:49:23 -0700

    more testing goodness

--- spec/convertSpec.js
+++ spec/convertSpec.js
@@ -5,16 +5,24 @@ describe("converty", function () {
     var result = new Conversion()
       .convert(1, "meter")
         .to("meter")
+        .execute();
 
-    debugger;
-    var result2 = result
-      .execute();
-
-    expect(result2).toBe(1, "Want the same result out as I put in");
+    expect(result).toBe(1, "Want the same result out as I put in");
 
   });
+  it("should let me convert inches to feet", function () {
+    var result = new Conversion().convert(12, "inches").to("feet").execute();
+    expect(result).toBe(1, "Want 1 foot coming out");
+  });
 
-  xit ("should throw an exception when I don't supply a .to", function () {
+  it("should let me convert feet to inches", function () {
+    var result = new Conversion().convert(1.5, "feet").to("inch").execute();
+    expect(result).toBe(18, "Want 18 inches for 1.5 feet");
+  });
+
+  it ("should throw an exception when I don't supply a .to", function () {
+    var halfsy = new Conversion().convert(1, "bomber");
+    expect( function () { halfsy.execute(); } ).toThrow(new Error("Failed to supply a target unit"));
   });
 
   xit ("should throw an exception when I don't supply a .convert", function () {
